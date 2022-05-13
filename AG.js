6c3df7702bc6f4c0d2cee4ca8f78626f8295e55c
Bialcizq <bialiczq@gmail.com>
2022-05-13 22:01:46 +0200

    create function for celumnDefs

--- AG.js
+++ AG.js
@@ -12,7 +12,16 @@ const rowData = [
     {  make: 'Aston Martin', model: 'DBX', price: 190000 },
 ];
 
-for (let i = 0; i > columnDefs.length; i++) {
-    console.log(columnDefs[i]);
+for (let i = 0; i < columnDefs.length; i++) {
+    //console.log(columnDefs[i]);
+    const divElement = document.createElement('div');
+    divElement.textContent = columnDefs[i].field;
+    document.querySelector('.headWrapper').appendChild(divElement);
+    for (let j = 0; j < rowData.length; j++) {
+        console.log('qwe');
+        const divElementData = document.createElement('div');
+        divElementData.textContent = rowData[j];
+        document.querySelector('div').appendChild(divElementData);
+    }
 }
 
