42a75ae777a020ecd3865cd7a780a57b39158ac7
Bialcizq <bialiczq@gmail.com>
2022-05-17 18:37:59 +0200

    create second table and buttons

--- AG.js
+++ AG.js
@@ -38,3 +38,21 @@ for (let i = 0; i < columnDefs.length; i++) {
     }
 }
 
+const secondWrapper = document.createElement('div')
+secondWrapper.className = 'newTable';
+document.querySelector('body').appendChild(secondWrapper)
+
+for (let y = 0; y < columnDefs.length; y++) {
+    const secondTableColumn = document.createElement('div');
+    const header = document.createElement('h1');
+    header.className = 'newTableColumnName';
+    header.textContent = columnDefs[y].field;
+    secondTableColumn.className = 'newTableColumn' + columnDefs[y].field;
+    document.querySelector('.newTable').appendChild(secondTableColumn);
+    document.querySelector('.newTableColumn' + columnDefs[y].field).appendChild(header);
+    for (let u = 0; u < rowData.length; u+=2) {
+        const newTableCell = document.createElement('div');
+        newTableCell.textContent = rowData[u][columnDefs[y].field];
+        document.querySelector('.newTableColumn' + columnDefs[y].field).appendChild(newTableCell);
+    }
+}
