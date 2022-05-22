6c6c1a4a6fb23485de7f59810a9008de35e56fd4
Bialcizq <bialiczq@gmail.com>
2022-05-22 23:05:58 +0200

    create table styles and order button

--- AG.js
+++ AG.js
@@ -1,6 +1,7 @@
 const sortByMake = document.querySelector('.sortByMake');
 const sortByModel = document.querySelector('.sortByModel');
 const sortByPrice = document.querySelector('.sortByPrice');
+const createOrder = document.querySelector('.createOrderButton');
 
 const newTableData = [];
 
@@ -32,6 +33,7 @@ for (let o = 0; o < rowData.length; o+=2) {
     newTableData.push(rowData[o]);
 }
 
+function createTable() {
 for (let i = 0; i < columnDefs.length; i++) {
     //console.log(columnDefs[i]);
     const divElement = document.createElement('div');
@@ -48,28 +50,30 @@ for (let i = 0; i < columnDefs.length; i++) {
         document.querySelector('.column' + columnDefs[i].field).appendChild(divElementData);
     }
 }
-
-const secondWrapper = document.createElement('div')
-secondWrapper.className = 'newTable';
-document.querySelector('body').appendChild(secondWrapper)
-
-for (let y = 0; y < columnDefs.length; y++) {
-    const secondTableColumn = document.createElement('div');
-    const header = document.createElement('h1');
-    header.className = 'newTableColumnName';
-    header.textContent = columnDefs[y].field;
-    secondTableColumn.className = 'newTableColumn' + columnDefs[y].field;
-    document.querySelector('.newTable').appendChild(secondTableColumn);
-    document.querySelector('.newTableColumn' + columnDefs[y].field).appendChild(header);
-    for (let u = 0; u < newTableData.length; u++) {
-        const newTableCell = document.createElement('div');
-        newTableCell.textContent = newTableData[u][columnDefs[y].field];
-        document.querySelector('.newTableColumn' + columnDefs[y].field).appendChild(newTableCell);
-    }
 }
 
+// const secondWrapper = document.createElement('div')
+// secondWrapper.className = 'newTable';
+// document.querySelector('body').appendChild(secondWrapper)
+
+// for (let y = 0; y < columnDefs.length; y++) {
+//     const secondTableColumn = document.createElement('div');
+//     const header = document.createElement('h1');
+//     header.className = 'newTableColumnName';
+//     header.textContent = columnDefs[y].field;
+//     secondTableColumn.className = 'newTableColumn' + columnDefs[y].field;
+//     document.querySelector('.newTable').appendChild(secondTableColumn);
+//     document.querySelector('.newTableColumn' + columnDefs[y].field).appendChild(header);
+//     for (let u = 0; u < newTableData.length; u++) {
+//         const newTableCell = document.createElement('div');
+//         newTableCell.textContent = newTableData[u][columnDefs[y].field];
+//         document.querySelector('.newTableColumn' + columnDefs[y].field).appendChild(newTableCell);
+//     }
+// }
+
+createTable();
+
 sortByMake.addEventListener('click', ()=> {
-    // console.log('qwe');
     rowData.sort((a, b)=> {
         if (a.make < b.make) {
             return -1
@@ -81,28 +85,11 @@ sortByMake.addEventListener('click', ()=> {
     });
     const emptyWrapper = document.querySelector('.headWrapper');
     emptyWrapper.innerHTML='';
-    console.log(rowData)
 
-    for (let i = 0; i < columnDefs.length; i++) {
-        //console.log(columnDefs[i]);
-        const divElement = document.createElement('div');
-        const header = document.createElement('h1');
-        header.className = 'columnName';
-        header.textContent = columnDefs[i].field;
-        divElement.className = 'column' + columnDefs[i].field;
-        document.querySelector('.headWrapper').appendChild(divElement);
-        document.querySelector('.column' + columnDefs[i].field).appendChild(header);
-        for (let j = 0; j < rowData.length; j++) {
-            // console.log('qwe');
-            const divElementData = document.createElement('div');
-            divElementData.textContent = rowData[j][columnDefs[i].field];
-            document.querySelector('.column' + columnDefs[i].field).appendChild(divElementData);
-        }
-    }
+    createTable();
 });
 
 sortByModel.addEventListener('click', ()=> {
-    console.log('qwe');
     rowData.sort((a, b)=> {
         if (a.model < b.model) {
             return -1
@@ -114,28 +101,11 @@ sortByModel.addEventListener('click', ()=> {
     });
     const emptyWrapper = document.querySelector('.headWrapper');
     emptyWrapper.innerHTML='';
-    console.log(rowData)
 
-    for (let i = 0; i < columnDefs.length; i++) {
-        //console.log(columnDefs[i]);
-        const divElement = document.createElement('div');
-        const header = document.createElement('h1');
-        header.className = 'columnName';
-        header.textContent = columnDefs[i].field;
-        divElement.className = 'column' + columnDefs[i].field;
-        document.querySelector('.headWrapper').appendChild(divElement);
-        document.querySelector('.column' + columnDefs[i].field).appendChild(header);
-        for (let j = 0; j < rowData.length; j++) {
-            // console.log('qwe');
-            const divElementData = document.createElement('div');
-            divElementData.textContent = rowData[j][columnDefs[i].field];
-            document.querySelector('.column' + columnDefs[i].field).appendChild(divElementData);
-        }
-    }
+    createTable();
 })
 
 sortByPrice.addEventListener('click', ()=> {
-    // console.log('qwe');
     rowData.sort((a, b)=> {
         if (a.price < b.price) {
             return -1
@@ -147,22 +117,24 @@ sortByPrice.addEventListener('click', ()=> {
     });
     const emptyWrapper = document.querySelector('.headWrapper');
     emptyWrapper.innerHTML='';
-    console.log(rowData)
 
-    // for (let i = 0; i < columnDefs.length; i++) {
-    //     //console.log(columnDefs[i]);
-    //     const divElement = document.createElement('div');
-    //     const header = document.createElement('h1');
-    //     header.className = 'columnName';
-    //     header.textContent = columnDefs[i].field;
-    //     divElement.className = 'column' + columnDefs[i].field;
-    //     document.querySelector('.headWrapper').appendChild(divElement);
-    //     document.querySelector('.column' + columnDefs[i].field).appendChild(header);
-    //     for (let j = 0; j < rowData.length; j++) {
-    //         // console.log('qwe');
-    //         const divElementData = document.createElement('div');
-    //         divElementData.textContent = rowData[j][columnDefs[i].field];
-    //         document.querySelector('.column' + columnDefs[i].field).appendChild(divElementData);
-    //     }
-    // }
+    createTable();
+})
+
+const amountData = [];
+
+createOrder.addEventListener('click', ()=> {
+    columnDefs.push({field: 'amount'});
+    
+    const emptyWrapper = document.querySelector('.headWrapper');
+    emptyWrapper.innerHTML='';
+
+    for (let  i = 0; i < rowData.length; i++) {
+        const amountElement = document.createElement('div');
+        const amountElementInput = document.createElement('input');
+        rowData[i].amount = amountElement;
+        amountElement.className = 'inputContainer';
+    }
+
+    createTable();
 })
